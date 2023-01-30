a3ff9964d039c8d4820798d787e97ec2c8c26d63
Van Dang Anh <vandait996@gmail.com>
2023-01-30 16:05:14 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.scss";
+import Countdown from "./components/countdown/Countdown";
+import Product from "./components/product/Product";
+
+function App() {
+  const [showCountdown, setShowCountdown] = useState(true)
+  return (
+    <div>
+      {showCountdown && <Countdown onHideCountdown={ () => setShowCountdown(false)} />}
+      
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
